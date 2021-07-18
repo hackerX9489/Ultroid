a94aca0d01f209b05ae361f1b9b0a3e90284ca79
hackerX9489 <83834904+hackerX9489@users.noreply.github.com>
2021-07-18 15:09:31 +0530

    Update ping.py

--- assistant/ping.py
+++ assistant/ping.py
@@ -16,5 +16,5 @@ async def _(event):
     ms = (end - start).microseconds / 1000
     await asst.send_message(
         event.chat_id,
-        f"**Pong!!**\n `{ms} milliseconds`",
+        f"Pong!!\n checked by\n Naren-@nn948\n `{ms} milliseconds`",
     )
